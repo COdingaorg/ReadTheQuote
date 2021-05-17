aa60ccb80b9d06c525b4aee6bc01d9ff1c263cfc
Caleb Odinga <calemasanga@gmail.com>
2021-05-17 14:03:22 +0300

    adds a new item to goal

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { formatCurrency } from '@angular/common';
+import { Component, OnInit, Output, EventEmitter   } from '@angular/core';
 import { QuoteClass } from '../quote-class';
 
 @Component({
@@ -7,14 +8,38 @@ import { QuoteClass } from '../quote-class';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-
+  //hardcodding place holder quote clases
+  newQuotesArray: any|QuoteClass[]=[
+    new QuoteClass('Iimothy', 'A bird in hand woth more than 2 in the bush', 'anonymous'),
+    new QuoteClass('lavender', 'what goes aoround...comes back around', 'Justin TImberlake')
+  ];
+  
   //create an initializing class
   newQuote = new QuoteClass('', '', '');
-  
-  ngOnInit(): void {
+
+  //adding the output decorator to addQuote
+  @Output() addQuote= new EventEmitter<QuoteClass>();
+
+  //submit button pushing a new item to quotes array
+ 
+  addQuotetoArray(){
+    this.newQuotesArray= this.newQuotesArray.push(this.newQuote)
+  };
+  addNewQuote(quote:any){
+    this.newQuotesArray.push(quote);
   }
 
+  // quotesListing(){
+  //   for(var i=0; i<=this.newQuotesArray.length; i++){
+  //     return this.newQuotesArray[i].quote;
+  //   }
+  // }
+  arrayLength:number = this.newQuotesArray.length;
+  ngOnInit(): void {
+    this.submitQuote();
+  }
   submitQuote(){
+    this.newQuotesArray.push(this.newQuote);
 
   }
 }
